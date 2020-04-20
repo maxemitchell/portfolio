75599fc97a0738cc8bb0878054df444fd7e8a89d
maxemitchell <maxemitchell@gmail.com>
2020-04-20 01:06:19 -0500

    fixing iPhone background color

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,11 +5,8 @@ import Helmet from 'react-helmet'
 const Layout = ({ children }) => {
   return (
     <div className="bg-themePurple text-themeOffWhite h-full min-h-screen antialiased">
-      <Helmet>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
-        />
+      <Helmet bodyAttributes={{style: 'background-color : #342e37'}}>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
       </Helmet>
       <Navigation />
       {children}
