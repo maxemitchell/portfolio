c65dd19d1a91d1bea28df255ebc32408d5294813
maxemitchell <maxemitchell@gmail.com>
2020-04-20 17:19:38 -0500

    Properly updated html and body background color

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,9 +5,6 @@ import Helmet from 'react-helmet'
 const Layout = ({ children }) => {
   return (
     <div className="bg-themePurple text-themeOffWhite h-full min-h-screen antialiased">
-      <Helmet bodyAttributes={{style: 'background-color : #342e37'}}>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
-      </Helmet>
       <Navigation />
       {children}
     </div>
